8f51dfdb2549d7080b8224f51c27afaeb1386b24
Kuisma Juho K <kujuku@dx7-biophys-04.physics.helsinki.fi>
2018-05-26 19:59:12 +0300

    More benchmarking

--- src/python/plot_benchmark.py
+++ src/python/plot_benchmark.py
@@ -2,14 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fpath = "../../output/benchmark/"
-files = ["gtx1070.dat", "i7.dat"]
+files = ["gtx1070.dat", "gtx1080.dat", "k80.dat", "i7.dat", "i5.dat"]
+labels = [
+    "GeForce GTX 1070",
+    "GeForce GTX 1080",
+    "Tesla K80",
+    "i7-7700K",
+    "i5-4200U"
+]
 
-for fname in files:
+for i,fname in enumerate(files):
     data = np.loadtxt(fpath + fname)
     size = data[:,0]
     t = data[:,1]
 
-    plt.semilogy(size, t/size**3)
+    plt.xlabel("Lattice size (n)")
+    plt.ylabel("Seconds per single spin flip")
+
+    plt.semilogy(size, t/size**3, label=labels[i])
+    plt.legend()
 
 plt.savefig("../../pics/benchmark")
 plt.show()
